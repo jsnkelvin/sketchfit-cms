4e5fa0472fd6a5f6a0a6c096ed2037b559b57629
Jason <jason.japutra@codify.id>
2020-03-09 09:07:53 +0700

    Add moment js, add checkbox on user list, implement edit and add voucher

--- src/app/pages/welcome/users/users.component.ts
+++ src/app/pages/welcome/users/users.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { differenceInCalendarYears } from 'date-fns';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users',
@@ -10,18 +11,66 @@ import { Router } from '@angular/router';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  @ViewChild('search', { static: true }) search: ElementRef;
 
   userData = [];
+  totalItems = 0;
+  pageIndex = 1;
+  currFilter = '';
+  currKeyword = '';
+  pageSize = '8';
+  searchValue;
   constructor(
     public http: HttpClient,
     public router: Router
   ) { }
 
   ngOnInit() {
-    this.http.get(environment.api_url + '/user?page=1&row=999').subscribe(data => {
-      console.log(data);
-      this.userData = data['result'];
+    fromEvent(this.search.nativeElement, 'keyup').pipe(
+      // get value
+      map((event: any) => {
+        return event.target.value;
+      })
+      , debounceTime(300)
+      , distinctUntilChanged()
+    ).subscribe((text: string) => {
+      this.currKeyword = text;
+      this.getUserList();
     });
+
+    this.getUserList();
+  }
+
+  getUserList() {
+    this.http.get(environment.api_url +
+      '/user?page=' + this.pageIndex +
+      '&row=' + this.pageSize +
+      '&search=' + this.currKeyword +
+      '&filter=' + this.currFilter)
+      .subscribe(success => {
+        console.log('SUCCESS', success);
+        this.userData = success['result'];
+        this.totalItems = success['count'];
+      }, err => {
+        console.log('ERROR', err);
+      });
+  }
+
+  indexChanged(event) {
+    this.pageIndex = event;
+    console.log('event', event);
+    this.http.get(environment.api_url +
+      '/user?page=' + event +
+      '&row=' + this.pageSize +
+      '&search=' + this.currKeyword +
+      '&filter=' + this.currFilter)
+      .subscribe(success => {
+        console.log('SUCCESS', success);
+        this.userData = success['result'];
+        this.totalItems = success['count'];
+      }, err => {
+        console.log('ERROR', err);
+      });
   }
 
 }
--- src/app/pages/welcome/voucher/voucher.component.ts
+++ src/app/pages/welcome/voucher/voucher.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-voucher',
@@ -10,19 +12,122 @@ import { environment } from 'src/environments/environment';
 export class VoucherComponent implements OnInit {
 
   voucherData = '';
+  currVoucher = '';
+  currFormMode = '';
+  isVisible = false;
+  isOkLoading = false;
+  validateForm: FormGroup;
+
+  demoValue = 100;
+  formatterPercent = (value: number) => `${value} %`;
+  parserPercent = (value: string) => value.replace(' %', '');
+  formatterDollar = (value: number) => `$ ${value}`;
+  parserDollar = (value: string) => value.replace('$ ', '');
+
   constructor(
-    public http:HttpClient
+    public http: HttpClient,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.getVoucherList();
+    this.validateForm = this.fb.group({
+      voucher_code: [null, [Validators.required]],
+      type: [null, [Validators.required]],
+      value: [0, [Validators.required]],
+      start_date: [null],
+      expire_date: [null],
+      limit: [0],
+      status: ['inactive', [Validators.required]],
+    });
+  }
+
+  getVoucherList() {
     this.http.get(environment.api_url + '/voucher?page=1&row=999').subscribe(data => {
       console.log(data);
       this.voucherData = data['result'];
     });
   }
 
-  editVoucher(voucher) {
-    console.log("Masuk edit", voucher)
+  showModal(type, voucher?) {
+    switch (type) {
+      case 'add': {
+        this.currFormMode = 'add';
+        this.resetForm();
+        this.isVisible = true;
+        break;
+      }
+      case 'edit': {
+        this.currFormMode = 'edit';
+        this.currVoucher = voucher;
+        this.validateForm.patchValue({
+          voucher_code: voucher.voucher_code,
+          type: voucher.type,
+          value: voucher.value,
+          start_date: moment(voucher.start_date).toISOString(),
+          expire_date: moment(voucher.expire_date).toISOString(),
+          limit: voucher.limit,
+          status: voucher.status
+        });
+        this.isVisible = true;
+        break;
+      }
+    }
   }
+
+  handleOk(): void {
+    console.log(this.validateForm.value);
+
+    this.isOkLoading = true;
+    switch (this.currFormMode) {
+      case 'add': {
+        this.http.post(environment.api_url + '/voucher', this.validateForm.value).subscribe(
+          success => {
+            console.log(success);
+            this.isVisible = false;
+            this.isOkLoading = false;
+            this.resetForm();
+            this.getVoucherList();
+          }, error => {
+            console.log(error);
+          }
+        );
+        break;
+      }
+      case 'edit': {
+        this.http.put(environment.api_url + '/voucher', this.validateForm.value).subscribe(
+          success => {
+            console.log(success);
+            this.isVisible = false;
+            this.isOkLoading = false;
+            this.getVoucherList();
+            this.resetForm();
+          }, error => {
+            console.log(error);
+          }
+        );
+        break;
+      }
+    }
+  }
+
+  handleCancel(): void {
+    this.currFormMode = '';
+    this.resetForm();
+    this.isVisible = false;
+  }
+
+  resetForm() {
+    this.validateForm.reset({
+      voucher_code: null,
+      type: null,
+      value: 0,
+      start_date: moment().toISOString(),
+      expire_date: moment().toISOString(),
+      limit: 0,
+      status: null
+    });
+  }
+
 
 }
