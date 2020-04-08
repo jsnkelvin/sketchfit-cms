99a3c49dfa100897213df3d5587061705aa56576
Jason <jason.japutra@codify.id>
2020-04-09 02:18:14 +0700

    Add qty field on voucher list, fix add and edit voucher

--- src/app/pages/welcome/voucher/voucher.component.ts
+++ src/app/pages/welcome/voucher/voucher.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
@@ -11,12 +11,15 @@ import * as moment from 'moment';
 })
 export class VoucherComponent implements OnInit {
 
+  @ViewChild('inputElement', { static: false }) inputElement: ElementRef;
   voucherData = '';
   currVoucher: any;
   currFormMode = '';
   isVisible = false;
   isOkLoading = false;
   validateForm: FormGroup;
+  value = '';
+  title = 'Input a number';
 
   demoValue = 100;
   formatterPercent = (value: number) => `${value} %`;
@@ -28,6 +31,46 @@ export class VoucherComponent implements OnInit {
     public http: HttpClient,
     private fb: FormBuilder
   ) { }
+
+  onChange(value: string): void {
+    this.updateValue(value);
+  }
+
+  // '.' at the end or only '-' in the input box.
+  onBlur(): void {
+    if (this.value.charAt(this.value.length - 1) === '.' || this.value === '-') {
+      this.updateValue(this.value.slice(0, -1));
+    }
+  }
+
+  updateValue(value: string): void {
+    const reg = /^-?(0|[1-9][0-9]*)(\.[0-9]*)?$/;
+    if ((!isNaN(+value) && reg.test(value)) || value === '' || value === '-') {
+      this.value = value;
+    }
+    this.inputElement.nativeElement.value = this.value;
+    this.updateTitle();
+  }
+
+  updateTitle(): void {
+    this.title = (this.value !== '-' ? this.formatNumber(this.value) : '-') || 'Input a number';
+  }
+
+  formatNumber(value: string): string {
+    const string = `${value}`;
+    const list = string.split('.');
+    const prefix = list[0].charAt(0) === '-' ? '-' : '';
+    let num = prefix ? list[0].slice(1) : list[0];
+    let result = '';
+    while (num.length > 3) {
+      result = `,${num.slice(-3)}${result}`;
+      num = num.slice(0, num.length - 3);
+    }
+    if (num) {
+      result = num + result;
+    }
+    return `${prefix}${result}${list[1] ? `.${list[1]}` : ''}`;
+  }
 
   ngOnInit() {
     this.getVoucherList();
@@ -45,7 +88,12 @@ export class VoucherComponent implements OnInit {
   getVoucherList() {
     this.http.get(environment.api_url + '/voucher?page=1&row=999').subscribe(data => {
       console.log(data);
+      data['result'].forEach(element => {
+        console.log(element);
+        element.updatedAt = moment(element.updatedAt).fromNow();
+      });
       this.voucherData = data['result'];
+      
     });
   }
 
@@ -60,6 +108,7 @@ export class VoucherComponent implements OnInit {
       case 'edit': {
         this.currFormMode = 'edit';
         this.currVoucher = voucher;
+        this.isVisible = true;
         this.validateForm.patchValue({
           voucher_code: voucher.voucher_code,
           type: voucher.type,
@@ -69,15 +118,12 @@ export class VoucherComponent implements OnInit {
           limit: voucher.limit,
           status: voucher.status
         });
-        this.isVisible = true;
         break;
       }
     }
   }
 
   handleOk(): void {
-    console.log(this.validateForm.value);
-
     this.isOkLoading = true;
     switch (this.currFormMode) {
       case 'add': {
